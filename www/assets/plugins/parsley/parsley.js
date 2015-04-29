e7b353dacf20cb64c2795f8003f567499d5a069b
firatyuksek <info@fymacmini.local>
2015-04-29 12:49:57 +0300

    UPDATE

--- www/assets/plugins/parsley/parsley.js
+++ www/assets/plugins/parsley/parsley.js
@@ -1830,28 +1830,28 @@ window.ParsleyConfig = window.ParsleyConfig || {};
 window.ParsleyConfig.i18n = window.ParsleyConfig.i18n || {};
 // Define then the messages
 window.ParsleyConfig.i18n.en = $.extend(window.ParsleyConfig.i18n.en || {}, {
-  defaultMessage: "This value seems to be invalid.",
+  defaultMessage: "Hatalı değer",
   type: {
-    email:        "This value should be a valid email.",
-    url:          "This value should be a valid url.",
-    number:       "This value should be a valid number.",
-    integer:      "This value should be a valid integer.",
-    digits:       "This value should be digits.",
-    alphanum:     "This value should be alphanumeric."
+    email:        "Hatalı eposta adresi.",
+    url:          "Hatalı URL.",
+    number:       "Değer rakam değil.",
+    integer:      "Değer tamsayı değil.",
+    digits:       "Digit hatalı.",
+    alphanum:     "Değer alphanumeric değil."
   },
-  notblank:       "This value should not be blank.",
-  required:       "This value is required.",
-  pattern:        "This value seems to be invalid.",
-  min:            "This value should be greater than or equal to %s.",
-  max:            "This value should be lower than or equal to %s.",
-  range:          "This value should be between %s and %s.",
-  minlength:      "This value is too short. It should have %s characters or more.",
-  maxlength:      "This value is too long. It should have %s characters or less.",
-  length:         "This value length is invalid. It should be between %s and %s characters long.",
-  mincheck:       "You must select at least %s choices.",
-  maxcheck:       "You must select %s choices or less.",
-  check:          "You must select between %s and %s choices.",
-  equalto:        "This value should be the same."
+  notblank:       "Bu alan boş kalamaz.",
+  required:       "Bu alan gerekli.",
+  pattern:        "Değer Hatalı.",
+  min:            "Büyük eşit olmalı, %s.",
+  max:            "Küçük eşit olmalı %s.",
+  range:          "Arasında olmalı %s ve %s.",
+  minlength:      "Değer çok kısa. Daha %s karakter girilmeli.",
+  maxlength:      "Değer çok uzun. Fazladan %s karakter var.",
+  length:         "Değer uzunluğu hatalı. Değer %s ve %s karakter arası olmalı.",
+  mincheck:       "En az  %s seçilmelidir.",
+  maxcheck:       "En fazla %s seçilebilir",
+  check:          " %s ve %s arası seçim gereklidir.",
+  equalto:        "Değerler birbirinin aynısı olmalıdır."
 });
 // If file is loaded after Parsley main file, auto-load locale
 if ('undefined' !== typeof window.ParsleyValidator)
